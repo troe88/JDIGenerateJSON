6ae6b353348cbc6cb4240b2693b2de34dbea7977
Dmitry Lebedev <dmitry_lebedev1@epam.com>
2015-11-09 16:06:08 +0300

    f1

--- plugin/toJava.js
+++ plugin/toJava.js
@@ -12,9 +12,11 @@ var templates = {
     IElement: "",
     ITextField: "",
     simpleClass: "\nclass {0} {\n{1}\n}",
-    IPagination: "public Pagination {0} = new Pagination({1});",
+    IPagination: "public Pagination {0} = new Pagination({1});\n",
     cfw: "\t@FindBy(css = \"[jdi-name={1}]\")\n\tpublic {0} {1};\n\n",
     cf: "\t{0} {1};\n",
+    fbcss: "\n\t{0},\n\t{1},\n\t{2},\n\t{3},\n\t{4}",
+    css: "By.cssSelector(\"[jdi-name={0}]\")"
 };
 
 var genFieldFormClass = {
@@ -41,16 +43,25 @@ var modelGenField = {
         process(data);
         return templates.cfw.format(data.name, "form");
     },
-    IPagination : function(data) {
-        return templates.IPagination.format(data.name, $.each(data.elements, function(index, value){
-
-        }));
+    IPagination: function (data) {
+        var elems = [];
+        var get = function (key) {
+            return (elems[key] === undefined) ? "null" : templates.css.format(elems[key].name)
+        };
+        $.each(data.elements, function (index, value) {
+            elems[value.name] = value;
+        });
+        return templates.IPagination.format(data.name, templates.fbcss.format(
+            get('template'), get('next'), get('prev'), get('first'), get('last')
+        ));
     },
-    ITimePicker : moduleSimple,
-    IDatePicker : moduleSimple,
+    ITimePicker: moduleSimple,
+    IDatePicker: moduleSimple,
     IPage: undefined,
     IElement: moduleSimple,
     ITextField: moduleSimple,
+    RFileInput : moduleSimple,
+    IRange : moduleSimple,
 };
 
 var modelComposite = {
@@ -92,7 +103,7 @@ function process(data) {
     modelComposite[data.type](data);
 }
 
-function translateToJava(data){
+function translateToJava(data) {
     filesArray = new Array;
     modelComposite[data.type](data);
     return filesArray;
